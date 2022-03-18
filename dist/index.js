a75d89ca825f0e6d83c8ec7c52e5266409e1d505
archaic10 <vitor.franca.9400@gmail.com>
2022-03-17 21:28:00 -0300

    refactor: refactor functions.

--- dist/index.js
+++ dist/index.js
@@ -8682,14 +8682,24 @@ async function getSHA(fileName){
     }).catch((error)=>{
         return error.status
     })
-    
-
 }
-async function uploadChangelog(content, fileName){
+
+async function deleteOldFile(param, fileName){
+    let token = githubToken
+    const octokit = new Octokit({ auth: token})
+    param.message = 'ci: Delete changelog'
+    await octokit.request('DELETE /repos/{owner}/{repo}/contents/{path}', param).then((res)=>{
+        delete param.sha;
+        console.log("Deletando arquivo: ", fileName)
+    }).catch((error)=>{
+        console.log("Erro ao deletar arquivo: ", fileName)
+    })
+    param.message = `ci: Update ${fileName}`
+}
+
+async function loadContentBase64(){
     let actor = github.context.actor
     let repository = github.context.payload.repository.name
-    let token = githubToken
-    const octokit = new Octokit({ auth: token});        
     let param;
     let sha = await getSHA(fileName);        
     param = {
@@ -8697,23 +8707,25 @@ async function uploadChangelog(content, fileName){
         repo: repository,
         path: fileName,
         message: `ci: Update ${fileName}`,
-        content: content
+        content: content,
+        sha: sha
     }
-    console.log(`status: ${sha} file ${fileName}`)
-    if(sha != 404 || fileName == 'package.json'){
-        param["sha"] = sha.data.sha;
-        console.log(`data ${fileName} : ${sha.data.sha}`)
-            
-                param.message = 'ci: Delete changelog'
-                await octokit.request('DELETE /repos/{owner}/{repo}/contents/{path}', param).then((res)=>{
-                    delete param.sha;
-                    console.log("Deletando arquivo: ", fileName)
-                }).catch((error)=>{
-                    console.log("Erro ao deletar arquivo: ", fileName)
-                })
-                param.message = `ci: Update ${fileName}`
-            
-    }
+    return param
+}
+
+async function recoveryShaConflict(error, param, fileName){
+    console.log("Recovery sha conflict: ", fileName)
+    let shaConflict = error.data.message;
+    shaConflict.split('expected').pop()
+    shaConflict = shaConflict.trim()
+    console.log("Sha recovery: ", shaConflict)
+    param["sha"] = shaConflict;
+    uploadFileBase64(param, fileName)
+}
+
+async function uploadFileBase64(param, fileName){
+    let token = githubToken
+    const octokit = new Octokit({ auth: token})
     await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', param).then((res)=>{
         console.log("Uploda arquivo: ", fileName)
         console.log({
@@ -8728,7 +8740,19 @@ async function uploadChangelog(content, fileName){
         
     }).catch(function(error){
         console.log("Error ao commitar file: ",error);
+        recoveryShaConflict(error, param, fileName)
     })
+}
+
+async function uploadChangelog(content, fileName){
+    console.log(`status: ${sha} file ${fileName}`)
+    let param = await loadContentBase64()
+    if(sha != 404 || fileName == 'package.json'){
+        param["sha"] = sha.data.sha;
+        console.log(`data ${fileName} : ${sha.data.sha}`)
+        deleteOldFile(param, fileName)
+    }
+    uploadFileBase64(param, fileName)
 }
 
 run()
