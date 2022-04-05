5416779db881af349c309a2ba09309f64462d6d2
archaic10 <vitor.franca.9400@gmail.com>
2022-04-05 17:32:44 -0300

    refactor: remove condiction package.json

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ async function uploadFileBase64(param, fileName){
 async function uploadGithub(content, fileName){
     let param = await loadContentBase64(fileName, content)    
     
-    if(param.sha != 404 || fileName == 'package.json'){
+    if(param.sha != 404){
         if(param.sha != 404)
             param["sha"] = param.sha.data.sha
         
